9891328ba07fa6bdceaccfa4b42a891b66225eb5
Himmat14 <himmat@allye.com>
2026-03-06 22:25:52 +0000

    create main

---
+++ main.py
@@ -0,0 +1,15 @@
+
+# Orbital Mechanics Coursework
+# Author: Himmat Kaul
+# Date: 2026-03-06
+# Description:
+
+
+
+
+# imprt libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.integrate import solve_ivp
+
+
